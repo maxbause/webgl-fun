044c0b3d42bcd1cb3817194e7d2b851de8115025
Max Bause <bause.max@territory.de>
2019-05-08 10:48:15 +0200

    added catch for browsers which does not support webgl

--- src/pages/basic-renderer.tsx
+++ src/pages/basic-renderer.tsx
@@ -3,12 +3,41 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const BasicRendererPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={["gatsby", "application", "react", "webgl", "programming", "basic", "renderer"]} />
-    <h1>Basic renderer</h1>
-    <canvas id="basic-renderer" width="400" height="300"></canvas>
-  </Layout>
-)
+interface IState {
+  webGLWarning: string;
+}
 
-export default BasicRendererPage
+export default class BasicRendererPage extends React.Component<{}, IState> {
+  private ref: React.RefObject<HTMLCanvasElement>;
+
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+    this.state = {
+      webGLWarning: "",
+    };
+  }
+
+  public componentDidMount() {
+    const gl = this.ref.current.getContext("webgl") || this.ref.current.getContext("experimental-webgl");
+    if (!gl) {
+      this.setState({
+        webGLWarning: "Sorry, but your browser does not suppport webgl."
+      })
+    }
+  }
+
+  public render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={["gatsby", "application", "react", "webgl", "programming", "basic", "renderer"]} />
+        <h1>Basic renderer</h1>
+        {this.state.webGLWarning ?
+          <p><i>{this.state.webGLWarning} ️️️😖</i></p>
+          :
+          <canvas id="basic-renderer" width="400" height="300" ref={this.ref}></canvas>
+        }
+      </Layout>
+    );
+  }
+}
